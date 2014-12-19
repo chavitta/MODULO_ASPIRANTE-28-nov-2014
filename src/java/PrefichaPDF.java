d34ceaebe2f1986de2d7109eda167703473ed53c
chavitta <chava_dalu5@hotmail.com>
2014-12-18 23:47:06 -0600

    PDF terminado: rutas dinamicas para las imagenes y acomodo de componentes para que se pueda imprimir mejor

--- src/java/PrefichaPDF.java
+++ src/java/PrefichaPDF.java
@@ -4,8 +4,6 @@
  * and open the template in the editor.
  */
 
-
-
 import ConexionBD.ConexionOracle;
 import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
@@ -19,6 +17,7 @@ import com.itextpdf.text.pdf.PdfGState;
 import com.itextpdf.text.pdf.PdfWriter;
 import java.awt.Font;
 import java.io.IOException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -28,6 +27,7 @@ import java.util.GregorianCalendar;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
+import static javax.servlet.SessionTrackingMode.URL;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -93,7 +93,7 @@ public class PrefichaPDF extends HttpServlet {
 //                documento.add(vacio);
                 Paragraph depto = new Paragraph("Departamento de servicios escolares", FontFactory.getFont("arial", 20, Font.BOLD));
                 depto.setAlignment(Element.ALIGN_CENTER);
-                
+
                 preficha.add(depto);
 
                 Paragraph vacio = new Paragraph("  ", FontFactory.getFont("arial", 10, Font.BOLD));
@@ -119,34 +119,30 @@ public class PrefichaPDF extends HttpServlet {
                 preficha.add(fotografia);
 
                 PdfContentByte rectangulo_general = writer.getDirectContentUnder();
-                rectangulo_general.rectangle(50, 28, 500, 730);
+                rectangulo_general.rectangle(50, 48, 500, 710);
                 rectangulo_general.fill();
-                drawRectangleSC(rectangulo_general, 50, 28, 500, 730);
+                drawRectangleSC(rectangulo_general, 50, 48, 500, 710);
 
                 PdfContentByte rectangulo_periodo = writer.getDirectContentUnder();
                 rectangulo_periodo.rectangle(125, 725, 350, 20);
                 rectangulo_periodo.fill();
                 drawRectangleSC(rectangulo_periodo, 125, 725, 350, 20);
+ 
+                String url_logo = "/Imagenes/itt_logo_opt.jpg";
+                String absolute_url_logo = getServletContext().getRealPath(url_logo);
+                Image itt_logo = Image.getInstance(absolute_url_logo);
 
-//                Image imagen = Image.getInstance(“hash_avatar.png”)
-                
-                
-                Image Logo_itt = Image.getInstance("C:\\Users\\Chavitta\\Documents\\GitHub\\MODULO_ASPIRANTE 28 nov 2014\\src\\java\\itt_logo_opt.jpg");
-                Logo_itt.setAbsolutePosition(140f,640f);
+                String url_logo_bnmx = "/Imagenes/bnmx_color_opt.jpg";
+                String absolute_url_logo_bnmx = getServletContext().getRealPath(url_logo_bnmx);
+                Image bnmx_logo = Image.getInstance(absolute_url_logo_bnmx);
+
+                Image Logo_itt = Image.getInstance(itt_logo);
+                Logo_itt.setAbsolutePosition(140f, 640f);
                 preficha.add(Logo_itt);
-                
-                Image Logo_banco = Image.getInstance("C:\\Users\\Chavitta\\Documents\\GitHub\\MODULO_ASPIRANTE 28 nov 2014\\src\\java\\bnmx_color_opt.jpg");
-                Logo_banco.setAbsolutePosition(380f,340f);
+
+                Image Logo_banco = Image.getInstance(bnmx_logo);
+                Logo_banco.setAbsolutePosition(380f, 340f);
                 preficha.add(Logo_banco);
-                
-                
-//                Image img = Image.getInstance("itt_logo.jpg");
-//                img.setAbsolutePosition(150f,650f);
-//                preficha.add(img);
-                
-                
-//              
-//                document.add(imagen);
                 
                 PdfContentByte espacio_imagen = writer.getDirectContentUnder();
                 espacio_imagen.rectangle(255, 635, 85, 80);
@@ -197,28 +193,24 @@ public class PrefichaPDF extends HttpServlet {
                 preficha.add(nombre);
 
 //                preficha.add(vacio);
-
-                Paragraph apellidos = new Paragraph("                                                                                               "+ apellidosbd,
+                Paragraph apellidos = new Paragraph("                                                                                               " + apellidosbd,
                         FontFactory.getFont("arial", 10, Font.BOLD));
                 apellidos.setAlignment(Element.ALIGN_LEFT);
                 preficha.add(apellidos);
 
 //                preficha.add(vacio);
-
                 Paragraph CURP = new Paragraph("                                                                                 CURP:   " + curpbd,
                         FontFactory.getFont("arial", 10, Font.BOLD));
                 CURP.setAlignment(Element.ALIGN_LEFT);
                 preficha.add(CURP);
 
 //                preficha.add(vacio);
-
                 Paragraph carrera = new Paragraph("                                                            Carrera Solicitada:   " + carrerabd,
                         FontFactory.getFont("arial", 10, Font.BOLD));
                 carrera.setAlignment(Element.ALIGN_LEFT);
                 preficha.add(carrera);
 
 //                preficha.add(vacio);
-
                 Paragraph modalidad = new Paragraph("                                                                          Modalidad:   " + modalidadbd,
                         FontFactory.getFont("arial", 10, Font.BOLD));
                 modalidad.setAlignment(Element.ALIGN_LEFT);
@@ -226,69 +218,68 @@ public class PrefichaPDF extends HttpServlet {
 
                 preficha.add(vacio);
 //                preficha.add(vacio);
-                
+
                 Paragraph formatoBanamex = new Paragraph("FORMATO UNIVERSAL PARA DEPÓSITOS EN SUCURSALES BANAMEX", FontFactory.getFont("arial", 10, Font.BOLD));
                 formatoBanamex.setAlignment(Element.ALIGN_CENTER);
                 preficha.add(formatoBanamex);
-                
+
                 PdfContentByte rectanguloDepositoB = writer.getDirectContentUnder();
                 rectanguloDepositoB.rectangle(85, 470, 430, 20);
                 rectanguloDepositoB.fill();
                 drawRectangle(rectanguloDepositoB, 85, 470, 430, 20);
-                
-                
+
                 PdfContentByte rectanguloPago = writer.getDirectContentUnder();
                 rectanguloPago.rectangle(85, 280, 430, 190);
                 rectanguloPago.fill();
                 drawRectangleSC(rectanguloPago, 85, 280, 430, 190);
-                
+
                 preficha.add(vacio);
-                
+
                 PdfContentByte rectanguloConcepto = writer.getDirectContentUnder();
-                rectanguloConcepto.rectangle(180, 425, 240, 35);
+                rectanguloConcepto.rectangle(150, 425, 295, 35);
 //                rectangulo_datos.rec
                 rectanguloConcepto.fill();
-                drawRectangleSC(rectanguloConcepto, 180, 425, 240, 35);
-                
-                Paragraph formatoConceptoPre = new Paragraph("CONCEPTO: PREINSCRIPCIONES", FontFactory.getFont("arial", 10, Font.BOLD));
+                drawRectangleSC(rectanguloConcepto, 150, 425, 295, 35);
+
+                Paragraph formatoConceptoPre = new Paragraph("CONCEPTO: PAGO DE DERECHO A EXAMEN DE ADMISIÓN", FontFactory.getFont("arial", 10, Font.BOLD));
                 formatoConceptoPre.setAlignment(Element.ALIGN_CENTER);
                 preficha.add(formatoConceptoPre);
-                
+
                 Paragraph fechaEmision = new Paragraph("FECHA DE EMISIÓN: 18/12/2014", FontFactory.getFont("arial", 10, Font.BOLD));
                 fechaEmision.setAlignment(Element.ALIGN_CENTER);
                 preficha.add(fechaEmision);
-                
+
                 preficha.add(vacio);
 //                preficha.add(vacio);
 //                preficha.add(vacio);
                 preficha.add(vacio);
-                
+
                 Paragraph importe = new Paragraph("IMPORTE A PAGAR: $1,500.°°", FontFactory.getFont("arial", 15, Font.BOLD));
                 importe.setAlignment(Element.ALIGN_CENTER);
                 preficha.add(importe);
-                
+
                 preficha.add(vacio);
                 preficha.add(vacio);
                 preficha.add(vacio);
 //                preficha.add(vacio);
 //                preficha.add(vacio);
-                
+
                 String ref = "44353452342353464765634523434";
-                
-                Paragraph referencia = new Paragraph("                                                   REFERENCIA (B): "+ref, FontFactory.getFont("arial", 10, Font.BOLD));
+
+                Paragraph referencia = new Paragraph("                                                   REFERENCIA (B): " + ref, FontFactory.getFont("arial", 10, Font.BOLD));
                 referencia.setAlignment(Element.ALIGN_LEFT);
                 preficha.add(referencia);
-                
+
                 preficha.add(vacio);
-                
+
                 Paragraph descrConcepto = new Paragraph("DESCRIPCIÓN DEL CONCEPTO: CUOTA POR CONCEPTO DE PREINCRIPCIÓN.", FontFactory.getFont("arial", 10, Font.BOLD));
                 descrConcepto.setAlignment(Element.ALIGN_CENTER);
                 preficha.add(descrConcepto);
-                
+
                 preficha.add(vacio);
                 preficha.add(vacio);
-                preficha.add(vacio);
-                preficha.add(vacio);
+//                preficha.add(vacio);
+//                preficha.add(vacio);
 //                preficha.add(vacio);
 //                preficha.add(vacio);
 
@@ -297,14 +288,14 @@ public class PrefichaPDF extends HttpServlet {
                 preficha.add(atencion);
 
                 PdfContentByte rectangulo_atencion = writer.getDirectContentUnder();
-                rectangulo_atencion.rectangle(245, 199, 100, 25);
+                rectangulo_atencion.rectangle(245, 229, 100, 25);
                 rectangulo_atencion.fill();
-                drawRectangle(rectangulo_atencion, 245, 199, 100, 25);
+                drawRectangle(rectangulo_atencion, 245, 229, 100, 25);
 
                 PdfContentByte rectangulo_info = writer.getDirectContentUnder();
-                rectangulo_info.rectangle(85, 65, 430, 100);
+                rectangulo_info.rectangle(85, 94, 430, 100);
                 rectangulo_info.fill();
-                drawRectangle(rectangulo_info, 85, 65, 430, 120);
+                drawRectangle(rectangulo_info, 85, 94, 430, 120);
 
                 preficha.add(vacio);
                 preficha.add(vacio);
@@ -351,7 +342,7 @@ public class PrefichaPDF extends HttpServlet {
         PdfGState state = new PdfGState();
         content.setGState(state);
         content.setRGBColorFill(232, 232, 232);
-        content.setColorStroke(BaseColor.CYAN);
+        content.setColorStroke(BaseColor.LIGHT_GRAY);
 //        content.setr
         content.setLineWidth((float) .5);
         content.rectangle(x, y, width, height);
@@ -377,7 +368,7 @@ public class PrefichaPDF extends HttpServlet {
 //        state.setFillOpacity(0.6f);
         content.setGState(state);
         content.setRGBColorFill(0xFF, 0xFF, 0xFA);
-        content.setColorStroke(BaseColor.CYAN);
+        content.setColorStroke(BaseColor.LIGHT_GRAY);
         content.setLineWidth((float) .5);
         content.rectangle(x, y, width, height);
         content.fillStroke();
@@ -389,7 +380,7 @@ public class PrefichaPDF extends HttpServlet {
         PdfGState state = new PdfGState();
         content.setGState(state);
         content.setRGBColorFill(0, 230, 255);
-        content.setColorStroke(BaseColor.CYAN);
+        content.setColorStroke(BaseColor.LIGHT_GRAY);
         content.setLineWidth((float) .5);
         content.rectangle(x, y, width, height);
         content.fillStroke();
